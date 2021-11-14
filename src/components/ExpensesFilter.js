d4657fb862f95159339add65214a40929582c6f5
niitish <nitish.headfirst@outlook.com>
2021-11-14 23:17:21 +0530

    de-abbreviate months

--- src/components/ExpensesFilter.js
+++ src/components/ExpensesFilter.js
@@ -11,18 +11,18 @@ const ExpenseFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by month</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value="Jan">Jan</option>
-          <option value="Feb">Feb</option>
-          <option value="Mar">Mar</option>
-          <option value="Apr">Apr</option>
+          <option value="January">January</option>
+          <option value="February">February</option>
+          <option value="March">March</option>
+          <option value="April">April</option>
           <option value="May">May</option>
-          <option value="Jun">Jun</option>
-          <option value="Jul">Jul</option>
-          <option value="Aug">Aug</option>
-          <option value="Sep">Sep</option>
-          <option value="Oct">Oct</option>
-          <option value="Nov">Nov</option>
-          <option value="Dec">Dec</option>
+          <option value="June">June</option>
+          <option value="July">July</option>
+          <option value="August">August</option>
+          <option value="September">September</option>
+          <option value="October">October</option>
+          <option value="November">November</option>
+          <option value="December">December</option>
         </select>
       </div>
     </div>
