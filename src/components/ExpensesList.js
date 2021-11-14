781411665b2242b0c9b1b233ee3871950ecc69a9
niitish <nitish.headfirst@outlook.com>
2021-11-14 23:18:08 +0530

    create ExpensesList.js

---
+++ src/components/ExpensesList.js
@@ -0,0 +1,22 @@
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">No expenses this month.</h2>;
+  }
+  return (
+    <div className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ExpensesList;
