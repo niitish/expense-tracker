6e64e3a71ce5ac26c271f56b50538717bded5b7b
niitish <nitish.headfirst@outlook.com>
2021-11-15 21:05:29 +0530

    add form open button

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,16 +1,38 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   const expenseDataHandler = (enteredData) => {
     const expenseData = {
       ...enteredData,
     };
+
     props.onExpenseAdd(expenseData);
+    setIsEditing(false);
   };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSave={expenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSave={expenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
